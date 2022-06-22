91b27b4d1ba29a94df2feab73ee6ee181adf103f
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-06-22 09:11:14 -0300

    organizing the project

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-
-  return (
-    <h1>Gift List</h1>
-  )
-  
-}
-
-export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+
+  return (
+    <h1>Presence List</h1>
+  )
+
+}
+
+export default Home
