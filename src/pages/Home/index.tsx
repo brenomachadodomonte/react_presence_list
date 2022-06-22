39736b5dde450121a683b31c32ae38da032ca49a
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-06-22 09:29:59 -0300

    Creating Style the Home Componet

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export function Home() {
+
+  return (
+    <div className='container'>
+      <h1>Presence List</h1>
+      <input type="text" placeholder="Type the name" />
+      <button type="button">Add</button>
+    </div>
+  )
+
+}
